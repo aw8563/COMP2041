af257ced2b56292ef95d87404d263218c01b3bec
Andy Wang <z5161179@student.unsw.edu.au>
2020-06-12 16:37:33 +1000

    scraping courses

---
+++ lab02/scraping_courses.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+undergraduate=$(curl --silent "http://legacy.handbook.unsw.edu.au/vbook2018/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=All")
+postgraduate=$(curl --silent "http://legacy.handbook.unsw.edu.au/vbook2018/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr=All")
+
+# echo $undergraduate > undergraduate
+# echo $postgraduate > postgraduate
+
+
+echo $undergraduate | sed "s/<TD class=\"\">/\n/g" | sed "s/<TD class=\"\" align=\"left\">/\n/g" | egrep "( |>|/)$1" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[/>]//g" | sed "s/<A//g" | sed "s/\.html\"/ /g" | grep -v "glossary"
+echo $postgraduate | sed "s/<TD class=\"\">/\n/g" | sed "s/<TD class=\"\" align=\"left\">/\n/g" | egrep "( |>|/)$1" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[/>]//g" | sed "s/<A//g" | sed "s/\.html\"/ /g" | grep -v "glossary"
+
+# cat out | egrep "( |>|/)OPTM" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[/>]//g" | sed "s/<A//g" | sed "s/\.html\"/ /g"
+
+
+# sed "s/<TD class=\"\">/\n/g" | sed "s/<TD class=\"\" align=\"left\">/\n/g" | egrep "( |>|/)MATH" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[/>]//g" | sed "s/<A//g" | sed "s/\.html\"/ /g"
