34f3efcb3802d6aadda49db105b952c56772a1e7
Andy Wang <z5161179@student.unsw.edu.au>
2020-06-12 17:27:09 +1000

    update

--- lab02/scraping_courses.sh
+++ lab02/scraping_courses.sh
@@ -7,10 +7,9 @@ postgraduate=$(curl --silent "http://legacy.handbook.unsw.edu.au/vbook2018/brCou
 # echo $postgraduate > postgraduate
 
 
-echo $undergraduate | sed "s/<TD class=\"\">/\n/g" | sed "s/<TD class=\"\" align=\"left\">/\n/g" | egrep "( |>|/)$1" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[/>]//g" | sed "s/<A//g" | sed "s/\.html\"/ /g" | grep -v "glossary"
-echo $postgraduate | sed "s/<TD class=\"\">/\n/g" | sed "s/<TD class=\"\" align=\"left\">/\n/g" | egrep "( |>|/)$1" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[/>]//g" | sed "s/<A//g" | sed "s/\.html\"/ /g" | grep -v "glossary"
+echo $undergraduate | sed "s/<TD class=\"\">/\n/g" | sed "s/<TD class=\"\" align=\"left\">/\n/g" | egrep "( |>|/)$1" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[>]//g" | sed "s/<\/A//g" | sed "s/^\///g" | sed "s/\.html\"/ /g" | grep -v "glossary"
+echo $postgraduate | sed "s/<TD class=\"\">/\n/g" | sed "s/<TD class=\"\" align=\"left\">/\n/g" | egrep "( |>|/)$1" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[>]//g" | sed "s/<\/A//g" | sed "s/^\///g" | sed "s/\.html\"/ /g" | grep -v "glossary"
 
-# cat out | egrep "( |>|/)OPTM" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[/>]//g" | sed "s/<A//g" | sed "s/\.html\"/ /g"
-
+# sed "s/<TD class=\"\">/\n/g" | sed "s/<TD class=\"\" align=\"left\">/\n/g" | egrep "( |>|/)MATH" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[>]//g" | sed "s/<\/A//g" | sed "s/^\///g" | sed "s/\.html\"/ /g" | grep -v "glossary"
 
 # sed "s/<TD class=\"\">/\n/g" | sed "s/<TD class=\"\" align=\"left\">/\n/g" | egrep "( |>|/)MATH" | egrep -o "/[^/]*\.html\">.*</A>" | cut -d ">" -f1,2 | sed "s/[/>]//g" | sed "s/<A//g" | sed "s/\.html\"/ /g"
